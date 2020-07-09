6ae9b6f4f18c8bd7b78219b758d37e977f5d41f7
Robert Carter <60618745+racarter1215@users.noreply.github.com>
2020-07-08 22:53:47 -0700

    Update Program.cs

--- Lab03-SystemI.O/Program.cs
+++ Lab03-SystemI.O/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Reflection.Metadata.Ecma335;
 
 namespace Lab03Challenge1
@@ -16,6 +16,7 @@ namespace Lab03Challenge1
             Console.WriteLine($"Here's the product for the numbers: {firstMethod}");
             Console.WriteLine("Enter a number between 2 and 10");
             string userAnswer = Console.ReadLine();
+            //this set of code lines basically makes throw exceptions for numbers that don't follow the criteria
             if (!int.TryParse(userAnswer, out int userNumber))
             {
                 throw new Exception("Please enter a number");
@@ -27,6 +28,7 @@ namespace Lab03Challenge1
             else
             {
                 int[] userArray = new int[userNumber];
+                //this for loop takes in the user data, parses it, and creates a variable out of it. It also manages a throw exception if the number is bad.
                 for (int i = 0; i < userArray.Length; i++)
                 {
                     Console.WriteLine($"Please enter a number {i + 1} of {userArray.Length}");
@@ -43,6 +45,7 @@ namespace Lab03Challenge1
                 double average = PickRandomNumberThenGetAverage(userArray);
                 Console.WriteLine($"The average number is {average}");
             }
+            //these code lines call the different methods I've done so far, plus console lines for user readability
             CreateDiamondDisplay(9);
             int bigValue = ArrayWithoutSortingIt(new int[] { 2, 2, 4, 6, 8, 8, 8 });
             Console.WriteLine($"The most common number in this array is {bigValue}");
@@ -54,16 +57,16 @@ namespace Lab03Challenge1
 
         }
         /// <summary>
-        /// The following code handles challenge 1, where 
+        /// The following code handles challenge 1, where you multiply 3 numbers, in essense
         /// </summary>
         /// <param name="input"></param>
         /// <returns></returns>
         public static int MultiplicationMethod(string input)
-        {
+        {//splits the string array into individual indexes for further manipulation
             string[] stringArray = input.Split(' ');
 
             if (stringArray.Length < 3)
-            {
+            {//says if its less than 3 return 0
                 return 0;
             }
 
@@ -72,22 +75,26 @@ namespace Lab03Challenge1
 
             for (int i = 0; i < 3; i++)
             {
-                //convert the string to an int
+                //convert the string to an integer
                 bool numberConversion = int.TryParse(stringArray[i], out int returnValue);
                 if (numberConversion)
                 {
-                    //if it's true
+                    //checks if it's true
                     product *= returnValue;
                 }
                 else
                 {
                     product *= 1;
                 }
-                //transfer to int
+                //transfer to integer
             }
             return product;
         }
-
+        /// <summary>
+        /// This method takes the array of numbers made in the Main method and generates an average
+        /// </summary>
+        /// <param name="userArray"></param>
+        /// <returns></returns>
         public static double PickRandomNumberThenGetAverage(int[] userArray)
         {
             int sum = 0;
@@ -95,15 +102,20 @@ namespace Lab03Challenge1
             {
                 sum += userArray[i];
             }
+            //creates variable average from sum divided by userarray.length
             double average = (double)sum / (double)userArray.Length;
             return average;
         }
-
+        /// <summary>
+        /// This method creates the diamond shape in the terminal
+        /// </summary>
+        /// <param name="rowLength"></param>
         public static void CreateDiamondDisplay(int rowLength)
-        {
+        {//below are named variables used within the code logic
             int center = ((rowLength + 1) / 2);
             string space = " ";
             char star = '*';
+            //the first "greater" for loop creates the top of the diamond
             for (int i = 1; i <= center; i++)
             {
                 for (int j = 1; j <= (center - i); j++)
@@ -116,7 +128,7 @@ namespace Lab03Challenge1
                 }
                 Console.WriteLine();
             }
-
+            //the bottom "greater" for loop creates the bottom half
             for (int i = center - 1; i > 0; i--)
             {
                 for (int j = 1; j <= (center - i); j++)
@@ -130,20 +142,25 @@ namespace Lab03Challenge1
                 Console.WriteLine();
             }
         }
-
+        /// <summary>
+        /// This method sorts and array without using .sort() and returns the largest number in the array
+        /// </summary>
+        /// <param name="totalNumberArray"></param>
+        /// <returns></returns>
         public static int ArrayWithoutSortingIt(int[] totalNumberArray)
-        {
+        {//below are the variable declarations for the code logic
             int numberCount = 0;
             int LargestNumberCount = 0;
             int firstNumberInArray = totalNumberArray[0];
             int numbersIteratedThrough = totalNumberArray[0];
+            //the outer for loop goes through the length of the array
             for (int i = 0; i < totalNumberArray.Length; i++)
             {
                 numberCount = 0;
                 for (int j = 0; j < totalNumberArray.Length; j++)
                 {
                     if (totalNumberArray[i] == totalNumberArray[j])
-                    {
+                    {//this adds one to the numberCount
                         numberCount += 1;
                     }
                     if (LargestNumberCount < numberCount)
@@ -155,10 +172,15 @@ namespace Lab03Challenge1
             }
             return firstNumberInArray;
         }
-
+        /// <summary>
+        /// This method finds the largest value in the array
+        /// </summary>
+        /// <param name="genericArray"></param>
+        /// <returns></returns>
         public static int MaximumValue(int[] genericArray)
         {
             int largestNumber = genericArray[0];
+            //this for loop looks for the most recent number and compares it to the next, over and over again, until the biggest one remains.
             for (int i = 0; i < genericArray.Length; i++)
             {
                 if (genericArray[i] > largestNumber)
